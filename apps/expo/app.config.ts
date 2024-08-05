b47efcbb5a86edf06ba2951d29ae98b14ffcbaac
Jethro Estrada <jethroestrada237@gmail.com>
2024-08-05 13:33:47 +0800

    Updated initial config for Digital Garage

--- apps/expo/app.config.ts
+++ apps/expo/app.config.ts
@@ -2,9 +2,9 @@ import type { ConfigContext, ExpoConfig } from "expo/config";
 
 export default ({ config }: ConfigContext): ExpoConfig => ({
   ...config,
-  name: "expo",
-  slug: "expo",
-  scheme: "expo",
+  name: "Digital Garage",
+  slug: "digital-garage",
+  scheme: "digitalgarage",
   version: "0.1.0",
   orientation: "portrait",
   icon: "./assets/icon.png",
@@ -19,11 +19,11 @@ export default ({ config }: ConfigContext): ExpoConfig => ({
   },
   assetBundlePatterns: ["**/*"],
   ios: {
-    bundleIdentifier: "your.bundle.identifier",
+    bundleIdentifier: "com.agileminds.digitalgarage.app",
     supportsTablet: true,
   },
   android: {
-    package: "your.bundle.identifier",
+    package: "com.agileminds.digitalgarage.app",
     adaptiveIcon: {
       foregroundImage: "./assets/icon.png",
       backgroundColor: "#1F104A",
@@ -39,4 +39,5 @@ export default ({ config }: ConfigContext): ExpoConfig => ({
     typedRoutes: true,
   },
   plugins: ["expo-router"],
+  jsEngine: "hermes",
 });
